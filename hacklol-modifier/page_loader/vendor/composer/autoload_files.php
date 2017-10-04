eb3334b22ac0183519ddcb43550a8174669855f0
Eliastik <eliastik@eliastiksofts.com>
2017-10-04 22:37:21 +0200

    Updated PHP proxy

--- hacklol-modifier/page_loader/vendor/composer/autoload_files.php
+++ hacklol-modifier/page_loader/vendor/composer/autoload_files.php
@@ -7,6 +7,6 @@ $baseDir = dirname($vendorDir);
 
 return array(
     '5e97df31a48c3d8473c36e3f1c45e0a4' => $vendorDir . '/athlon1600/youtube-downloader/src/YouTubeDownloader.php',
-    '311a7016008bd2d1bccdd3da08cf87ee' => $vendorDir . '/athlon1600/php-proxy-plugin-bundle/src/utils.php',
     'fe17454461a24db888b8da8720edd309' => $vendorDir . '/athlon1600/php-proxy/src/helpers.php',
+    '311a7016008bd2d1bccdd3da08cf87ee' => $vendorDir . '/athlon1600/php-proxy-plugin-bundle/src/utils.php',
 );
