36267df032abf1557d13a33ccbc43961fc11366f
Eliastik <eliastik@eliastiksofts.com>
2018-01-03 18:30:02 +0100

    Fix

--- hacklol-modifier/index.php
+++ hacklol-modifier/index.php
@@ -1,20 +1,20 @@
 <?php
 /* Hacklol Modifier 1.4
- * 
- * Copyright (C) 2014-2017 Eliastik (eliastiksofts.com)
- * 
+ *
+ * Copyright (C) 2014-2018 Eliastik (eliastiksofts.com)
+ *
  * This file is part of Hacklol Modifier.
- * 
+ *
  * Hacklol Modifier is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * Hacklol Modifier is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with Hacklol Modifier.  If not, see <http://www.gnu.org/licenses/>.
  *  */
@@ -206,40 +206,44 @@ function isValid($code, $ip = null)
     return $json->success;
 }
 // https://stackoverflow.com/questions/1634782/what-is-the-most-accurate-way-to-retrieve-a-users-correct-ip-address-in-php
- function get_ip() {
-  // Check for shared internet/ISP IP
-  if (!empty($_SERVER['HTTP_CLIENT_IP']) && validate_ip($_SERVER['HTTP_CLIENT_IP'])) {
-   return $_SERVER['HTTP_CLIENT_IP'];
-  }
-  // Check for IPs passing through proxies
-  if (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-   // Check if multiple IP addresses exist in var
-    $iplist = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
-    foreach ($iplist as $ip) {
-     if (validate_ip($ip))
-      return $ip;
+function get_ip() {
+    // Check for shared internet/ISP IP
+    if (!empty($_SERVER['HTTP_CLIENT_IP']) && validate_ip($_SERVER['HTTP_CLIENT_IP'])) {
+        return $_SERVER['HTTP_CLIENT_IP'];
     }
-   }
-  if (!empty($_SERVER['HTTP_X_FORWARDED']) && validate_ip($_SERVER['HTTP_X_FORWARDED']))
-   return $_SERVER['HTTP_X_FORWARDED'];
-  if (!empty($_SERVER['HTTP_X_CLUSTER_CLIENT_IP']) && validate_ip($_SERVER['HTTP_X_CLUSTER_CLIENT_IP']))
-   return $_SERVER['HTTP_X_CLUSTER_CLIENT_IP'];
-  if (!empty($_SERVER['HTTP_FORWARDED_FOR']) && validate_ip($_SERVER['HTTP_FORWARDED_FOR']))
-   return $_SERVER['HTTP_FORWARDED_FOR'];
-  if (!empty($_SERVER['HTTP_FORWARDED']) && validate_ip($_SERVER['HTTP_FORWARDED']))
-   return $_SERVER['HTTP_FORWARDED'];
-  // Return unreliable IP address since all else failed
-  return $_SERVER['REMOTE_ADDR'];
+
+    // Check for IPs passing through proxies
+    if (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        // Check if multiple IP addresses exist in var
+        $iplist = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
+
+        foreach ($iplist as $ip) {
+            if (validate_ip($ip))
+            return $ip;
+        }
+    }
+
+    if (!empty($_SERVER['HTTP_X_FORWARDED']) && validate_ip($_SERVER['HTTP_X_FORWARDED']))
+    return $_SERVER['HTTP_X_FORWARDED'];
+
+    if (!empty($_SERVER['HTTP_X_CLUSTER_CLIENT_IP']) && validate_ip($_SERVER['HTTP_X_CLUSTER_CLIENT_IP']))
+    return $_SERVER['HTTP_X_CLUSTER_CLIENT_IP'];
+
+    if (!empty($_SERVER['HTTP_FORWARDED_FOR']) && validate_ip($_SERVER['HTTP_FORWARDED_FOR']))
+    return $_SERVER['HTTP_FORWARDED_FOR'];
+
+    if (!empty($_SERVER['HTTP_FORWARDED']) && validate_ip($_SERVER['HTTP_FORWARDED']))
+    return $_SERVER['HTTP_FORWARDED'];
+
+    // Return unreliable IP address since all else failed
+    return $_SERVER['REMOTE_ADDR'];
 }
+
 function validate_ip($ip) {
-     if (filter_var($ip, FILTER_VALIDATE_IP,
-                         FILTER_FLAG_IPV4 |
-                         FILTER_FLAG_IPV6 |
-                         FILTER_FLAG_NO_PRIV_RANGE |
-                         FILTER_FLAG_NO_RES_RANGE) === false)
-         return false;
-     $ip = $ip;
-     return true;
+    if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4 | FILTER_FLAG_IPV6 | FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE) === false)
+    return false;
+    $ip = $ip;
+    return true;
 }
 // Pour savoir si l'URL est une adresse IP
 function is_url_ip($url) {
--- hacklol-modifier/page_loader/plugins/ProtectPlugin.php
+++ hacklol-modifier/page_loader/plugins/ProtectPlugin.php
@@ -1,6 +1,6 @@
 <?php
-/* ProtectPlugin pour Hacklol Modifier, by Eliastik
-   Dernière modification : 24/10/2017 - Version 1.2 */
+/* ProtectPlugin for Hacklol Modifier, by Eliastik
+   Latest modification: 03/01/2018 - Version 1.2.1 */
 
 use Proxy\Plugin\AbstractPlugin;
 use Proxy\Event\ProxyEvent;
@@ -90,39 +90,43 @@ function is_url_ip($url) {
     }
 }
 // https://stackoverflow.com/questions/1634782/what-is-the-most-accurate-way-to-retrieve-a-users-correct-ip-address-in-php
- function get_ip() {
-  // Check for shared internet/ISP IP
-  if (!empty($_SERVER['HTTP_CLIENT_IP']) && validate_ip($_SERVER['HTTP_CLIENT_IP'])) {
-   return $_SERVER['HTTP_CLIENT_IP'];
-  }
-  // Check for IPs passing through proxies
-  if (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-   // Check if multiple IP addresses exist in var
-    $iplist = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
-    foreach ($iplist as $ip) {
-     if (validate_ip($ip))
-      return $ip;
+function get_ip() {
+    // Check for shared internet/ISP IP
+    if (!empty($_SERVER['HTTP_CLIENT_IP']) && validate_ip($_SERVER['HTTP_CLIENT_IP'])) {
+        return $_SERVER['HTTP_CLIENT_IP'];
     }
-   }
-  if (!empty($_SERVER['HTTP_X_FORWARDED']) && validate_ip($_SERVER['HTTP_X_FORWARDED']))
-   return $_SERVER['HTTP_X_FORWARDED'];
-  if (!empty($_SERVER['HTTP_X_CLUSTER_CLIENT_IP']) && validate_ip($_SERVER['HTTP_X_CLUSTER_CLIENT_IP']))
-   return $_SERVER['HTTP_X_CLUSTER_CLIENT_IP'];
-  if (!empty($_SERVER['HTTP_FORWARDED_FOR']) && validate_ip($_SERVER['HTTP_FORWARDED_FOR']))
-   return $_SERVER['HTTP_FORWARDED_FOR'];
-  if (!empty($_SERVER['HTTP_FORWARDED']) && validate_ip($_SERVER['HTTP_FORWARDED']))
-   return $_SERVER['HTTP_FORWARDED'];
-  // Return unreliable IP address since all else failed
-  return $_SERVER['REMOTE_ADDR'];
- }
- function validate_ip($ip) {
-     if (filter_var($ip, FILTER_VALIDATE_IP,
-                         FILTER_FLAG_IPV4 |
-                         FILTER_FLAG_IPV6 |
-                         FILTER_FLAG_NO_PRIV_RANGE |
-                         FILTER_FLAG_NO_RES_RANGE) === false)
-         return false;
-     $ip = $ip;
-     return true;
- }
+
+    // Check for IPs passing through proxies
+    if (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        // Check if multiple IP addresses exist in var
+        $iplist = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
+
+        foreach ($iplist as $ip) {
+            if (validate_ip($ip))
+            return $ip;
+        }
+    }
+
+    if (!empty($_SERVER['HTTP_X_FORWARDED']) && validate_ip($_SERVER['HTTP_X_FORWARDED']))
+    return $_SERVER['HTTP_X_FORWARDED'];
+
+    if (!empty($_SERVER['HTTP_X_CLUSTER_CLIENT_IP']) && validate_ip($_SERVER['HTTP_X_CLUSTER_CLIENT_IP']))
+    return $_SERVER['HTTP_X_CLUSTER_CLIENT_IP'];
+
+    if (!empty($_SERVER['HTTP_FORWARDED_FOR']) && validate_ip($_SERVER['HTTP_FORWARDED_FOR']))
+    return $_SERVER['HTTP_FORWARDED_FOR'];
+
+    if (!empty($_SERVER['HTTP_FORWARDED']) && validate_ip($_SERVER['HTTP_FORWARDED']))
+    return $_SERVER['HTTP_FORWARDED'];
+
+    // Return unreliable IP address since all else failed
+    return $_SERVER['REMOTE_ADDR'];
+}
+
+function validate_ip($ip) {
+    if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4 | FILTER_FLAG_IPV6 | FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE) === false)
+    return false;
+    $ip = $ip;
+    return true;
+}
 ?>
