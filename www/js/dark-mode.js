28df56629509be50a64f660cd0b0a35907ec49a4
Eliastik <26941242+Eliastik@users.noreply.github.com>
2026-06-10 23:23:01 +0200

    Display current theme

--- www/js/dark-mode.js
+++ www/js/dark-mode.js
@@ -36,10 +36,8 @@
       return
     }
 
-    const themeSwitcherText = document.querySelector('#bd-theme-text')
     const activeThemeIcon = document.querySelector('.theme-icon-active use')
     const btnToActive = document.querySelector(`[data-bs-theme-value="${theme}"]`)
-    const svgOfActiveBtn = btnToActive.querySelector('svg use').getAttribute('href')
 
     document.querySelectorAll('[data-bs-theme-value]').forEach(element => {
       element.classList.remove('active')
@@ -48,9 +46,6 @@
 
     btnToActive.classList.add('active')
     btnToActive.setAttribute('aria-pressed', 'true')
-    activeThemeIcon.setAttribute('href', svgOfActiveBtn)
-    const themeSwitcherLabel = `${themeSwitcherText.textContent} (${btnToActive.dataset.bsThemeValue})`
-    themeSwitcher.setAttribute('aria-label', themeSwitcherLabel)
 
     if (focus) {
       themeSwitcher.focus()
